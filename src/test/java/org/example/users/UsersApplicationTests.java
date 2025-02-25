bfdfe384f73c06a7c5925ee410b8a77bb5719f6b
QUIRINO <quirino228je@gmail.com>
2025-02-25 20:29:07 +0200

    Test pipline

--- src/test/java/org/example/users/UsersApplicationTests.java
+++ src/test/java/org/example/users/UsersApplicationTests.java
@@ -1,30 +1,26 @@
 package org.example.users;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
+import org.example.users.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
-@ActiveProfiles("test") // This will use application-test.yml
+@ActiveProfiles("test")
 class UsersApplicationTests {
 
-    @BeforeAll
-    static void setup() {
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-
-        System.setProperty("POSTGRES_HOST", dotenv.get("POSTGRES_HOST", "localhost"));
-        System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT", "5432"));
-        System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB", "testdb"));
-        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER", "sa"));
-        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD", ""));
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testDatabaseConnection() {
+        assertNotNull(userRepository);
     }
 }
